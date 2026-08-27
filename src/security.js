d2e0d22178c5d4a068f17063d0a243cb8136069c
elonwilliammusk426-arch <elonwilliammusk426@gmail.com>
2026-08-27 01:09:49 -0700

    security: add application security middleware

---
+++ src/security.js
@@ -0,0 +1,15 @@
+const helmet = require('helmet');
+
+function securityMiddleware(app) {
+  app.disable('x-powered-by');
+  app.use(helmet());
+}
+
+function requireConfiguredDatabase(req, res, next) {
+  if (process.env.NODE_ENV === 'production' && !process.env.DATABASE_URL) {
+    return res.status(503).json({ error: 'Service is not configured for production' });
+  }
+  next();
+}
+
+module.exports = { securityMiddleware, requireConfiguredDatabase };
